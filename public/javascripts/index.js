0c5d7a178278444a8586a33e2d5f5bad78ea9350
liufulong <liufulong@douyu.tv>
2018-05-16 10:13:19 +0800

    增加动态展示

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,13 @@
 (function () {
-    setTimeout(() => {
-        document.getElementById('js-ele').innerHTML = '动态内容：by buynao';
-    }, 1000);
+    var $body = document.body;
+    var str = '动态内容：';
+    for (var i = 1; i <= 12; i++) {  
+        (function(T) {  
+            setTimeout( function timer() {
+                var $p = document.createElement('p')
+                $p.innerHTML = '<p>' + str + '页面加载'  + T * 1000 + 'ms后展示</p>';
+                $body.appendChild($p);  
+            }, i*1000 );  
+        })(i);  
+    }  
 })();
